aa9161aabd99f0eecc133e96022796f71df15792
yangjichun182 <390835164@qq.com>
2018-07-23 13:48:54 +0800

    20180723

---
+++ src/main/java/com/pactera/servlet/RegisterServlet.java
@@ -0,0 +1,71 @@
+package com.pactera.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+
+		// 1 获取到请求的数据内容:username
+		String username = request.getParameter("username");
+
+		System.out.println("username = " + username);
+
+		// 2 查询数据库:1)结果集为空,用户名可用;2)结果集不为空,用户名不可用
+
+		// 模拟查询数据库
+		if (username == null || "".equals(username)) {
+			out.println("请输入你的用户名!");
+		} else if (username.equals("sa")) {
+			out.println("该用户名已存在!");
+		} else {
+			out.println("该用户名可使用!");
+		}
+
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to
+	 * post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+
+	}
+
+}
