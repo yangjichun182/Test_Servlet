6b81eefffcdffe74432eaa0f42791955b15f0f9b
yangjichun182 <390835164@qq.com>
2021-01-19 15:21:05 +0800

    创建utils包下的说明

---
+++ src/main/java/com/jichun/utils/UtilsIllustration.java
@@ -0,0 +1,18 @@
+package com.jichun.utils;
+
+// 常用日期处理工具类
+import com.jichun.utils.DateUtils;
+
+/**
+ * @function 
+ * @author xbm
+ * @date   2021年1月19日 下午3:07:11
+ *
+ */
+public class UtilsIllustration {
+
+	public static void main(String[] args) {
+
+	}
+
+}
